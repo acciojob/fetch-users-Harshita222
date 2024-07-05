26d7d8ca3dc8c7c7b48f17ea1e2d34c2e07fc85c
Harshita222 <kumariharsita501@gmail.com>
2024-07-05 22:19:30 +0530

    does

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from "react";
+
+import 'regenerator-runtime/runtime';
+import React from "react"; 
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
